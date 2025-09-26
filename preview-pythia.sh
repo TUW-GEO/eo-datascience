a00a169a9863036957c357f48b50ff9dbd7b23f6
Martin Schobben <martin.schobben@geo.tuwien.ac.at>
2025-09-26 13:38:51 +0200

    [fix] misc

--- preview-pythia.sh
+++ preview-pythia.sh
@@ -6,14 +6,18 @@ pythia_convert --out ./${1}
 ./copy-pythia.sh ./${1}
 ./download-pythia.sh ./${1}
 
-read -p "User name for HSAF: " USER_HSAF
-read -p "Password for HSAF user $USER_HSAF: " PASS_HSAF
-read -p "User name for WEkEO: " USER_WEKEO
-read -p "Password for WEkEO user $USER_WEKEO: " PASS_WEKEO
-
 ENV_PATH=./${1}/notebooks/courses/environmental-remote-sensing/unit_01/.env
-touch $ENV_PATH
-echo USER_HSAF = $USER_HSAF >> $ENV_PATH
-echo PASS_HSAF = $PASS_HSAF >> $ENV_PATH
-echo USER_WEKEO = $USER_WEKEO>> $ENV_PATH
-echo PASS_WEKEO = $PASS_WEKEO >> $ENV_PATH
+
+if [ ! -e "$ENV_PATH" ]; then
+    echo "File not found: $ENV_PATH"
+    echo "Executing command: $*"
+    touch $ENV_PATH
+    echo USER_HSAF = $USER_HSAF >> $ENV_PATH
+    echo PASS_HSAF = $PASS_HSAF >> $ENV_PATH
+    echo USER_WEKEO = $USER_WEKEO>> $ENV_PATH
+    echo PASS_WEKEO = $PASS_WEKEO >> $ENV_PATH
+else
+    echo "File already exists: $ENV_PATH"
+    echo "Command skipped"
+    exit 0
+fi
